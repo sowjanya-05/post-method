c4c912718ca912dac145e57a294491d442f687e2
pbsc <pbsc@csclab1sys122.parjanya.pbsiddhartha.ac.in>
2022-11-24 17:15:20 +0530

    24-11=2022

---
+++ index.js
@@ -0,0 +1,28 @@
+const express=require("express")
+const app=express()
+const cors=require("cors")
+const bodyParser=require("body-parser")
+const port=2500;
+
+app.use(bodyParser.urlencoded({
+	extended:true
+}))
+
+app.use(bodyParser.json())
+
+app.use(cors())
+
+app.get("/",(req,res)=>{
+	res.send("empty route")
+})
+
+app.post("/newData",(req,res)=>{
+	//console.log(req.body)
+	//const name=req.body.name,age=req.body.age
+	const {name,password}=req.body;
+	console.log(name,password)
+	res.send("added")
+})
+
+
+app.listen(port,()=>console.log("server is running on port 2500"))
